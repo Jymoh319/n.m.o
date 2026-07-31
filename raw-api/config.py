89a301280bd9770fd963f5ab981f97ebc44b4477
ryanmakori <ryanndigitalsolutionz@gmail.com>
2026-07-31 15:19:03 +0300

    changed SQLite URI to PostgreSQL URL for deployment!

--- raw-api/config.py
+++ raw-api/config.py
@@ -1,7 +1,7 @@
 import os
 
 class Config:
-    SQLALCHEMY_DATABASE_URI = "sqlite:///demo.db"
+    SQLALCHEMY_DATABASE_URI = os.getenv("DATABASE_URL", "sqlite:///demo.db")
     SQLALCHEMY_TRACK_MODIFICATIONS = False
     SECRET_KEY = os.getenv("SECRET_KEY", "playlist-secret-key")
     JWT_SECRET_KEY = os.getenv("JWT_SECRET_KEY", "jwt-secret-key")

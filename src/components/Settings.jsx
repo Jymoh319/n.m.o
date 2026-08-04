b8b9e4bfe892b2fd0be9830857a02facfca9353d
Emmanuel Sum <esumkipkorir@gmail.com>
2026-08-05 00:54:59 +0300

    fixed settings

--- src/components/Settings.jsx
+++ src/components/Settings.jsx
@@ -1,4 +1,4 @@
-settings.jsx                                                                                                   import { Link } from "react-router-dom";
+                                                                                                import { Link } from "react-router-dom";
 import { useState, useEffect } from "react";
 import {
   Sun,
